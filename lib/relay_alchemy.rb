066d43185c261febfce7f6b2b2869acf8bf5ba70
David Conner <dconner.pro@gmail.com>
2013-06-02 05:24:05 -0600

    module to handle default options for api requests

---
+++ lib/relay_alchemy.rb
@@ -0,0 +1,31 @@
+module RelayAlchemy 
+
+  module TextSentimentAnalysis
+    
+    def self.defaults
+      { 'outputMode' => 'json',
+        'url' => '',
+        'showSourceText' => 0.to_s }
+    end
+
+    def self.build_params(p = {})
+      defaults.merge(text: p['text'], url: p['url']).symbolize_keys
+    end
+
+  end
+
+end
+ 
+# Response Format (JSON):
+
+#       {
+#     "status": "REQUEST_STATUS",
+#     "url": "REQUESTED_URL",
+#     "language": "DOCUMENT_LANGUAGE",
+#     "text": "DOCUMENT_TEXT",
+#       "docSentiment": {
+#         "type": "SENTIMENT_LABEL",
+#         "score": "DOCUMENT_SENTIMENT",
+#         "mixed": "SENTIMENT_MIXED"
+#       }
+#     }
